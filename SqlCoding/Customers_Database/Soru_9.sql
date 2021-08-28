c589276f4e067ab8d7fa05849544fc4d460b86bf
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-28 12:11:49 +0300

    added Soru_9

---
+++ SqlCoding/Customers_Database/Soru_9.sql
@@ -0,0 +1,7 @@
+﻿/*
+Hangi şehirde kaç erkek, kaç kadın müşterimizin olduğunu sıralı bir şekilde bulunuz.
+*/
+select ct.City,c.Gender,COUNT(c.Id) as CustomerCount from Customers c
+inner join Cities ct on c.CityId=ct.Id
+group by ct.City,c.Gender
+order by ct.City
