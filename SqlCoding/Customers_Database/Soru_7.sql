f981023c6d3f23b3c951302ed8442f8a7837377e
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-28 12:11:02 +0300

    added Soru_7

---
+++ SqlCoding/Customers_Database/Soru_7.sql
@@ -0,0 +1,6 @@
+﻿/*
+Hangi şehirde kaç müşterimizin olduğu bilgisini getiren sorguyu yazınız.
+*/
+select ct.City,count(c.Id) as CustomerCount from Customers c
+right join Cities ct on c.CityId=ct.Id
+group by ct.City
