25a1751c74c03fa74c5e0d98b62907a5633fa5fc
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-28 12:11:19 +0300

    added Soru_8

---
+++ SqlCoding/Customers_Database/Soru_8.sql
@@ -0,0 +1,8 @@
+﻿/*
+10'dan fazla müşterimiz olan şehirleri müşteri sayısı ile birlikte müşteri sayısına göre fazladan aza doğru sıralı şekilde getiriniz.
+*/
+select ct.City,count(c.Id) as CustomerCount from Customers c
+inner join Cities ct on c.CityId=ct.Id
+group by ct.City
+having count(c.Id)>10
+order by CustomerCount desc
