1cfab5e1d65050f895930afe059368da2fcfad5a
Besir Gunduz <besirgunduz1993@gmail.com>
2021-09-01 19:57:10 +0300

    added Soru_13

---
+++ SqlCoding/Customers_Database/Soru_13.sql
@@ -0,0 +1,15 @@
+﻿/*
+istanbul'da yaşayıp ilçesi 'Kadıköy' dışında olanları listeleyiniz.
+*/
+select * from Customers c
+inner join Cities ct on c.CityId=ct.Id
+inner join District d on c.DistrictId=d.Id
+where ct.City='İstanbul' and d.District <> 'Kadıköy'
+
+-------2.Yöntem-----------------------
+
+select * from Customers
+where CityId = (select Id from Cities where City='İstanbul') and
+      DistrictId <> (select Id from District where District='Kadıköy')
+      
+
