1b54078eb0b39d6dd40a19e6724567cc9f4b4d83
Besir Gunduz <besirgunduz1993@gmail.com>
2021-09-01 19:57:24 +0300

    added Soru_14

---
+++ SqlCoding/Customers_Database/Soru_14.sql
@@ -0,0 +1,10 @@
+﻿/*
+Cities tablosundan 'Ankara' kaydını sildiğimizi varsayalım.Bu durumda şehri
+'Ankara' olan müşterilerin şehir alanı boş gelecektir.
+Şehir alanı boş olan müşterileri listeleyen sorguyu yazınız.
+*/
+delete from Cities where City='Ankara'
+
+select * from Customers c
+left join Cities ct on c.CityId=ct.Id
+where ct.City is null
