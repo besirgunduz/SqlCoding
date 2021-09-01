924f7643010ff5922ca2b6d6a1ab7bd614d72f13
Besir Gunduz <besirgunduz1993@gmail.com>
2021-09-01 19:55:59 +0300

    updated Soru_9

--- SqlCoding/Customers_Database/Soru_9.sql
+++ SqlCoding/Customers_Database/Soru_9.sql
@@ -5,3 +5,11 @@ select ct.City,c.Gender,COUNT(c.Id) as CustomerCount from Customers c
 inner join Cities ct on c.CityId=ct.Id
 group by ct.City,c.Gender
 order by ct.City
+
+--------------2.Yöntem-----------------------------
+
+select ct.City,
+(select COUNT(c.Id) from Customers c where c.CityId=ct.Id and c.Gender='E'),
+(select COUNT(c.Id) from Customers c where c.CityId=ct.Id and c.Gender='K')
+from Cities ct
+
