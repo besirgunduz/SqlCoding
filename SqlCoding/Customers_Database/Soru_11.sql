467719edb4b1ab9cb4a64779d1593c38626063f2
Besir Gunduz <besirgunduz1993@gmail.com>
2021-09-01 19:56:41 +0300

    added Soru_11

---
+++ SqlCoding/Customers_Database/Soru_11.sql
@@ -0,0 +1,6 @@
+﻿/*
+Customers tablosunda AgeGroup alanını '20-35 yaş arası' olarak güncelleyiniz. 
+*/
+update Customers
+set AgeGroup = '20-35 yaş arası'
+where DATEDIFF(YEAR,Birthdate,GETDATE()) between 20 and 35
