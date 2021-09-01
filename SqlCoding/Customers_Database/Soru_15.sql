2bd41365211f803120507d5388834095c5e41460
Besir Gunduz <besirgunduz1993@gmail.com>
2021-09-01 20:00:50 +0300

    added Soru_15

--- SqlCoding/Customers_Database/Soru_15.sql
+++
@@ -1,7 +0,0 @@
-﻿/*
-Cities tablosundan 'Ankara' şehrini sildikten sonra aynı Id ile tabloya 'Ankara' şehrini ekleyiniz
-*/
-delete from Cities where City='Ankara'
-
-set identity_insert Cities on
-insert into Cities(Id,City) values(6,'Ankara')
