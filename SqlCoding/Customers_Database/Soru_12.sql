299ceb88c3574c6f8ff63f79a15e3bccf27c8901
Besir Gunduz <besirgunduz1993@gmail.com>
2021-09-01 19:56:55 +0300

    added Soru_12

---
+++ SqlCoding/Customers_Database/Soru_12.sql
@@ -0,0 +1,8 @@
+﻿/*
+Customers tablosunda müşterilerin yaş grubuna göre hesaplayınız.
+*/
+select AgeGroup,Count(Id) from customers
+group by AgeGroup
+
+---------2.Yöntem---------------------------
+--TODO
