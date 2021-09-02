b8c07d1e81f6775d0a86e3054c74652d60439278
Besir Gunduz <besirgunduz1993@gmail.com>
2021-09-02 12:38:32 +0300

    added Soru_16

---
+++ SqlCoding/Customers_Database/Soru_15.sql
@@ -0,0 +1,9 @@
+﻿/*
+Müşterilerimizin telefon numaralarının operatör bilgisini getirmek istiyoruz.
+Telnr1 ve Telnr2 alanlarının yanına operatör numarasını (532),(505) gibi getirmek 
+istiyoruz.Bu sorgu için sql cümlesini yazınız.
+*/
+select 
+telnr1,substring(TelNr1,2,3) as operator1,
+telnr1,substring(TelNr2,2,3) as operator2
+from Customers
