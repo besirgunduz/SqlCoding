100777993724e593c9a44bb1218d05751af4f9b0
Besir Gunduz <besirgunduz1993@gmail.com>
2022-09-21 14:24:48 +0300

    CustomersWhoNeverOrder

---
+++ SqlCoding/CombineTwoTables.sql
@@ -0,0 +1,5 @@
+﻿-- Kişi tablosundaki her kişinin adını, soyadını, şehrini ve durumunu bildirmek için bir SQL sorgusu yazın. 
+-- Adres tablosunda bir kişi kimliğinin adresi yoksa, bunun yerine null yazsın.
+
+select p.firstName,p.lastName,a.city,a.[state] from Person p 
+left join Address a on p.PersonId = a.PersonId
