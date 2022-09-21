d4cd2b3f28f2a87b23012a1852555c4c10a388ce
Besir Gunduz <besirgunduz1993@gmail.com>
2022-09-21 10:21:08 +0300

    SecondHighestSalary

--- SqlCoding/SecondHighestSalary.sql
+++ SqlCoding/SecondHighestSalary.sql
@@ -2,3 +2,7 @@
 
 select max(salary) as SecondHighestSalary  from Employee
 where salary<(select max(salary) from Employee);
+
+--OR
+
+select max(e2.salary) from Employee e1 inner join Employee e2 on e1.salary>e2.salary;
